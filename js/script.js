aed80de81c78db84ebe738b02cb924e9afdee0e5
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2022-10-04 13:53:55 -0300

    updated all

--- js/script.js
+++ js/script.js
@@ -2,18 +2,41 @@ const inputEmail = document.querySelector('#inputEmail');
 const inputPassword = document.querySelector('#inputPassword');
 const btnLogin = document.querySelector('.btn_sign_in');
 
-    btnLogin.addEventListener('click', (e)=>{
-        e.preventDefault()    
-        const email = inputEmail;
-        const password = inputPassword.value;
-            if(email === '');{
-                inputEmail.classList.add('errorInput');
-                console.log('Email vazio')
+btnLogin.addEventListener('click', (e) => {
+    e.preventDefault()
+    const email = inputEmail.value;
+    const password = inputPassword.value;
+    if (email === '') {
+        inputEmail.classList.toggle('errorInput');
+        console.log('Era o ; mesmo')
 
+    }
+    if (password === '') {
+        inputPassword.classList.toggle('errorInput')
+        console.log('Senha vazio')
+    }
+    if (email && password != '') {
+        inputEmail.classList.remove('errorInput');
+        inputPassword.classList.remove('errorInput')
+
+    }
+
+    function inputClickFill() {
+
+        inputEmail.addEventListener('click', (e) => {
+            e.preventDefault()
+            if (inputEmail.classList === 'errorInput') {
+                console.log('Remover class errorInput');
             }
-            if(password === ''){
-                inputPassword.classList.add('errorInput')
-                console.log('Senha vazio')
-            }
-            }
-    );
+            inputEmail.classList.add = '';
+        })
+
+
+    }
+
+
+}
+
+);
+
+
